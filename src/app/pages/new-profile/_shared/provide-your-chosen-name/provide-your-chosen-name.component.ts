3f9d41208e7ed477517f71e23a4b2cca44122b68
André Lagacione <lagacioneandre@gmail.com>
2024-02-23 18:10:49 -0500

    your profile name

--- src/app/pages/new-profile/_shared/provide-your-chosen-name/provide-your-chosen-name.component.ts
+++ src/app/pages/new-profile/_shared/provide-your-chosen-name/provide-your-chosen-name.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { ProfileState } from 'src/app/states/state/profile.state';
 import { UpdateProfileAction } from 'src/app/states/actions/update-profile.action';
 import { Subject, takeUntil } from 'rxjs';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HandleError } from 'src/app/commons/handle-error/handle-error';
+import { ProfileUpdate } from 'src/app/commons/services/profile-update.service';
 
 @Component({
   selector: 'app-provide-your-chosen-name',
   templateUrl: './provide-your-chosen-name.component.html',
   styleUrls: ['./provide-your-chosen-name.component.scss']
 })
-export class ProvideYourChosenNameComponent implements OnDestroy, OnInit {
+export class ProvideYourChosenNameComponent extends HandleError implements OnDestroy, OnInit {
 
   private destroy$ = new Subject<boolean>();
+  private readonly profileUpdate = inject(ProfileUpdate);
   color = '#7A87CC';
   imageFile = '';
   userName = 'Chosen Name';
@@ -25,7 +28,9 @@ export class ProvideYourChosenNameComponent implements OnDestroy, OnInit {
   constructor(
     private router: Router,
     private store: Store
-  ) {}
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.listenProfileState();
@@ -60,8 +65,21 @@ export class ProvideYourChosenNameComponent implements OnDestroy, OnInit {
       ...profile,
       userName: this.name?.value,
     };
-    this.store.dispatch( new UpdateProfileAction(profileUpdated) );
-    this.router.navigate(['/new-profile/chosen-profile-name']);
+
+    this.profileUpdate.update({ chosenName: this.name?.value })
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        next: () => {
+          this.store.dispatch( new UpdateProfileAction(profileUpdated) );
+          this.router.navigate(['/new-profile/chosen-profile-name']);
+        },
+        error: _error => {
+          super.handleError(_error);
+          this.isLoading = false;
+        },
+      });
   }
 
   private listenInputChanges() {
--- src/app/pages/new-profile/_shared/provide-your-chosen-name/provide-your-chosen-name.module.ts
+++ src/app/pages/new-profile/_shared/provide-your-chosen-name/provide-your-chosen-name.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProvideYourChosenNameComponent } from './provide-your-chosen-name.component';
 import { ProvideYourChosenNameRoutingModule } from './provide-your-chosen-name-routing';
-import { InputValidationModule, ProfilePreviewModule } from 'millez-web-components/dist/components';
+import { InputValidationModule, LoadingButtonModule, ProfilePreviewModule, SpinnerModule } from 'millez-web-components/dist/components';
 import { ProfileState } from 'src/app/states/state/profile.state';
 import { NgxsModule } from '@ngxs/store';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NgxsModule.forRoot([
       ProfileState
     ]),
+    SpinnerModule,
+    LoadingButtonModule,
+  ],
+  providers: [
+    TranslatePipe,
   ]
 })
 export class ProvideYourChosenNameModule { }
