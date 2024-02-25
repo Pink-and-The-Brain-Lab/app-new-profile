d2a48157ef464ff6f5843c1a33e0c99a58fc95c4
André Lagacione <lagacioneandre@gmail.com>
2024-02-25 17:39:55 -0500

    set profile type

--- src/app/pages/new-profile/_shared/select-profile-type/select-profile-type.component.ts
+++ src/app/pages/new-profile/_shared/select-profile-type/select-profile-type.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { ProfileState } from 'src/app/states/state/profile.state';
 import { UpdateProfileAction } from 'src/app/states/actions/update-profile.action';
 import { Subject, takeUntil } from 'rxjs';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProfileType } from './models/profile-type.interface';
+import { HandleError } from 'src/app/commons/handle-error/handle-error';
+import { ProfileUpdate } from 'src/app/commons/services/profile-update.service';
 
 @Component({
   selector: 'app-select-profile-type',
   templateUrl: './select-profile-type.component.html',
   styleUrls: ['./select-profile-type.component.scss']
 })
-export class SelectProfileTypeComponent implements OnDestroy, OnInit {
+export class SelectProfileTypeComponent extends HandleError implements OnDestroy, OnInit {
 
   private destroy$ = new Subject<boolean>();
+  private readonly profileUpdate = inject(ProfileUpdate);
   color = '#7A87CC';
   imageFile = '';
   userName = 'Chosen Name';
   profileName = 'profile name';
-  isLoading = false;
   profileOptions: ProfileType[] = [{
     value: 'Private',
     label: 'Private',
@@ -29,18 +30,15 @@ export class SelectProfileTypeComponent implements OnDestroy, OnInit {
   }];
   selectedOption = this.profileOptions[1];
 
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(4)])
-  });
-
   constructor(
     private router: Router,
     private store: Store
-  ) {}
+    ) {
+      super();
+    }
 
   ngOnInit(): void {
     this.listenProfileState();
-    this.listenInputChanges();
   }
 
   ngOnDestroy(): void {
@@ -76,26 +74,23 @@ export class SelectProfileTypeComponent implements OnDestroy, OnInit {
     };
     this.store.dispatch( new UpdateProfileAction(profileUpdated) );
     
-
-    // TODO - fazer a chamada da API para salvar os dados do profile
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 2000);
-  }
-
-  private listenInputChanges() {
-    this.form.get('name')?.valueChanges
+    this.profileUpdate.update({ profileVisibility: this.selectedOption.value })
       .pipe(
         takeUntil(this.destroy$)
       )
-      .subscribe(response => this.profileName = response?.trim() || 'profile name')
+      .subscribe({
+        next: () => {
+          this.store.dispatch( new UpdateProfileAction(profileUpdated) );
+          this.router.navigate(['/']);
+        },
+        error: _error => {
+          super.handleError(_error);
+          this.isLoading = false;
+        },
+      });
   }
 
   getSelectedPropfileOption(value: ProfileType) {
-    console.log(value);
-  }
-
-  get name(): AbstractControl | null {
-    return this.form.get('name');
+    this.selectedOption = value;
   }
 }
--- src/app/pages/new-profile/_shared/select-profile-type/select-profile-type.module.ts
+++ src/app/pages/new-profile/_shared/select-profile-type/select-profile-type.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { SelectProfileTypeComponent } from './select-profile-type.component';
 import { SelectProfileTypeRoutingModule } from './select-profile-type-routing';
 import { CustomSelectModule, LoadingButtonModule, ProfilePreviewModule, SpinnerModule } from 'millez-web-components/dist/components';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxsModule } from '@ngxs/store';
 import { ProfileState } from 'src/app/states/state/profile.state';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -15,14 +15,15 @@ import { ProfileState } from 'src/app/states/state/profile.state';
     CommonModule,
     SelectProfileTypeRoutingModule,
     ProfilePreviewModule,
-    FormsModule,
-    ReactiveFormsModule,
     CustomSelectModule,
     LoadingButtonModule,
     SpinnerModule,
     NgxsModule.forRoot([
       ProfileState
     ]),
+  ],
+  providers: [
+    TranslatePipe,
   ]
 })
 export class SelectProfileTypeModule { }
