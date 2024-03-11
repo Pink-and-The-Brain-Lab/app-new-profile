634bc293e6ba7fbb1980166f7eafaeafa40f7d15
André Lagacione <lagacioneandre@gmail.com>
2024-03-11 15:16:38 -0400

    remove profile state

--- src/app/states/actions/update-profile.action.ts
+++
@@ -1,6 +0,0 @@
-import { IProfile } from "../models/profile.interface";
-
-export class UpdateProfileAction {
-    static readonly type = '[Profile] UpdateProfile';
-    constructor (public profile: IProfile) {}
-}
