ef9e1c7478d6ff389d20f2d185aa8ed8a188e2e6
André Lagacione <lagacioneandre@gmail.com>
2024-03-18 20:49:34 -0400

    tests

--- src/app/pages/new-profile/_shared/select-theme/select-theme.component.spec.ts
+++ src/app/pages/new-profile/_shared/select-theme/select-theme.component.spec.ts
@@ -5,20 +5,23 @@ import { ChooseNavyProfileIllustrationModule } from 'src/app/illustrations/choos
 import { ChooseLightProfileIllustrationModule } from 'src/app/illustrations/choose-light-profile-illustration/choose-light-profile-illustration.module';
 import { ChoosePurpleProfileIllustrationModule } from 'src/app/illustrations/choose-purple-profile-illustration/choose-purple-profile-illustration.module';
 import { ChooseLightPurpleProfileIllustrationModule } from 'src/app/illustrations/choose-light-purple-profile-illustration/choose-light-purple-profile-illustration.module';
-import { LoadingButtonModule, SpinnerModule } from 'millez-web-components/dist/components';
+import { LoadingButtonModule, SpinnerModule, Theme, ThemeChangerService } from 'millez-web-components/dist/components';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { ToastrService } from 'ngx-toastr';
 import { ProfileUpdate } from 'src/app/commons/services/profile-update.service';
 import TOASTR_SERVICE_MOCK from 'src/app/mocks/toastr-service.test.mock';
 import PROFILE_UPDATE_MOCK from 'src/app/mocks/profile-update-service.test.mock';
+import { Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
 
 describe('SelectThemeComponent', () => {
   let component: SelectThemeComponent;
   let fixture: ComponentFixture<SelectThemeComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -47,10 +50,45 @@ describe('SelectThemeComponent', () => {
 
     fixture = TestBed.createComponent(SelectThemeComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate').and.stub();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to previous page', () => {
+    component.back();
+    expect(router.navigate).toHaveBeenCalledWith(['/new-profile/phone-number-validation']);
+  });
+
+  it('should set selectedTheme and call loadTheme with correct theme', () => {
+    const theme = 'dark' as any;
+    const themeChangerService = TestBed.inject(ThemeChangerService);
+    const spy = spyOn(themeChangerService, 'loadTheme');
+    component.selectTheme(theme);
+    expect(spy).toHaveBeenCalled();
+  });
+  
+  it('should save profile data and go to the next page', () => {
+    const store = TestBed.inject(Store);
+    const storeSpy = spyOn(store, 'selectSnapshot').and.returnValue({});
+    const service = TestBed.inject(ProfileUpdate);
+    const serviceSpy = spyOn(service, 'update').and.returnValue(of({} as any));
+    component.next();
+    expect(storeSpy).toHaveBeenCalled();
+    expect(serviceSpy).toHaveBeenCalled();
+  });
+  
+  it('should get an error save profile data and go to the next page', () => {
+    const store = TestBed.inject(Store);
+    const storeSpy = spyOn(store, 'selectSnapshot').and.returnValue({});
+    const service = TestBed.inject(ProfileUpdate);
+    const serviceSpy = spyOn(service, 'update').and.returnValue(throwError(() => ({error: {message: ''}})));
+    component.next();
+    expect(storeSpy).toHaveBeenCalled();
+    expect(serviceSpy).toHaveBeenCalled();
   });
 });
