1c358995f6749e0ec43fa8db753286cb700696b0
André Lagacione <lagacioneandre@gmail.com>
2024-03-19 20:11:49 -0400

    unit tests

--- src/app/pages/new-profile/new-profile.component.spec.ts
+++ src/app/pages/new-profile/new-profile.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NewProfileRoutingModule } from './new-profile-routing.module';
 import { NewProfileComponent } from './new-profile.component';
+import { LogoModule } from 'millez-web-components/dist/components';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { NgxsModule, Store } from '@ngxs/store';
+import { ToastrService } from 'ngx-toastr';
+import { ProfileUpdate } from 'src/app/commons/services/profile-update.service';
+import TOASTR_SERVICE_MOCK from 'src/app/mocks/toastr-service.test.mock';
+import PROFILE_UPDATE_MOCK from 'src/app/mocks/profile-update-service.test.mock';
+import { Router } from '@angular/router';
 
 describe('NewProfileComponent', () => {
   let component: NewProfileComponent;
@@ -9,7 +19,19 @@ describe('NewProfileComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ NewProfileComponent ],
-      imports: [NewProfileRoutingModule]
+      imports: [
+        LogoModule,
+        HttpClientTestingModule,
+        NoopAnimationsModule,
+        RouterTestingModule.withRoutes([]),
+        TranslateModule.forRoot(),
+        NgxsModule.forRoot([]),
+      ],
+      providers: [
+        TranslatePipe,
+        { provide: ToastrService, useValue: TOASTR_SERVICE_MOCK },
+        { provide: ProfileUpdate, useValue: PROFILE_UPDATE_MOCK },
+      ]
     })
     .compileComponents();
 
@@ -20,5 +42,15 @@ describe('NewProfileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close the profile configuration', () => {
+    const router = TestBed.inject(Router);
+    const routerSpy = spyOn(router, 'navigate');
+    const store = TestBed.inject(Store);
+    const storeSpy = spyOn(store, 'dispatch');
+    component.close();
+    expect(routerSpy).toHaveBeenCalled();
+    expect(storeSpy).toHaveBeenCalled();
   });
 });
